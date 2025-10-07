6fbf8c6178eee2ddee12a2569739839fa4252dfd
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2025-10-07 17:08:34 -0400

    Update nor2.vhd
    Updated to make it fully structural by instantiating primitives

--- VHDL/nor2.vhd
+++ VHDL/nor2.vhd
@@ -1,20 +1,41 @@
 -- Entity: nor2
 -- Architecture: structural
 -- Author: Juan Marroquin
---
-library STD;
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity nor2 is
   port (
-    input1   : in  std_logic;
-    input2   : in  std_logic;
-    output   : out std_logic);
-end not2;
+    input1 : in  std_logic;
+    input2 : in  std_logic;
+    output : out std_logic
+  );
+end nor2;
 
 architecture structural of nor2 is
-  
+
+  component or2
+    port (
+      input1 : in  std_logic;
+      input2 : in  std_logic;
+      output : out std_logic
+    );
+  end component;
+
+  component inverter
+    port (
+      input : in  std_logic;
+      output: out std_logic
+    );
+  end component;
+
+  signal temp: std_logic;
+  for or2_1: or2 use entity work.or2(structural);
+  for inv1: inverter use entity work.inverter(structural);
+
 begin
-  output <= not (input2 or input1);
+  or2_1: or2 port map (input1, input2, temp);
+  inv1: inverter port map (temp, output);
+
 end structural;

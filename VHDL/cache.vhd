4bc60534effe2ea1ef545ac5f9d008055c1ef357
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2025-10-24 13:37:47 -0400

    Update cache.vhd

--- VHDL/cache.vhd
+++ VHDL/cache.vhd
@@ -31,8 +31,7 @@ architectural structural of cache is
           reset           : in  std_logic;
           start           : in  std_logic;
           read_write      : in  std_logic;  
-          cvt             : in  std_logic;  
-  
+          cvt             : in  std_logic; 
           busy            : out std_logic;
           output_enable   : out std_logic;
           write_enable    : out std_logic;
@@ -43,6 +42,70 @@ architectural structural of cache is
           byte_cnt_out : out std_logic_vector(1 downto 0)
       );
   end component;
+
+component cache_data_block is
+  port (
+        CE_index     : in std_logic_vector(3 downto 0);
+        CE_offset    : in std_logic_vector(3 downto 0);
+        RD_WR  : in std_logic;
+        Tag_in : in std_logic_vector(1 downto 0);
+        Tag_out: out std_logic_vector(1 downto 0);
+        V_in   : in std_logic;
+        V_out  : out std_logic;
+        D_in   : in std_logic_vector(7 downto 0);
+        D_out  : out std_logic_vector(7 downto 0));
+end component;
+--------------------------------------------------------------------------------
+-- DECODER AND MUXES
+--------------------------------------------------------------------------------
+component decoder2to4 is 
+    port (
+        EN    : in std_logic;
+        A    : in std_logic_vector(1 downto 0);
+        Y  : out std_logic_vector(3 downto 0));
+end component;
+        
+component mux2to1_8bit is
+  port (
+    S   : in  std_logic;
+    I0  : in  std_logic_vector(7 downto 0);
+    I1  : in  std_logic_vector(7 downto 0);
+    Y   : out std_logic_vector(7 downto 0)
+  );
+end component;
+
+component mux2to1_2bit is
+  port (
+    S   : in  std_logic;
+    I0  : in  std_logic_vector(1 downto 0);
+    I1  : in  std_logic_vector(1 downto 0);
+    Y   : out std_logic_vector(1 downto 0)
+  );
+end component;
+
+--------------------------------------------------------------------------------
+-- REGISTERS AND HELPERS
+--------------------------------------------------------------------------------
+component reg8 is
+  port (
+    D    : in  std_logic_vector(7 downto 0);
+    CLK  : in  std_logic;
+    RESET: in  std_logic;
+    Q    : out std_logic_vector(7 downto 0)
+  );
+end component;
+       
+component cvt is
+  port (
+    input1   : in std_logic_vector(1 downto 0); -- input Tag[1:0]
+    input2   : in std_logic_vector(1 downto 0); -- input cache Tag[1:0]
+    valid   : in std_logic; -- valid bit
+    output   : out std_logic); -- hit / miss
+end component;
+
+        
+
+    
 
   
       

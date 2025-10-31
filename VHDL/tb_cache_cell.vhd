4d45cd99ed01eb509a2c3fdf222421dbe9ea8269
hannahMathew1 <168139287+hannahMathew1@users.noreply.github.com>
2025-10-31 18:03:11 -0400

    Update tb_cache_cell.vhd

--- VHDL/tb_cache_cell.vhd
+++ VHDL/tb_cache_cell.vhd
@@ -11,10 +11,11 @@ entity tb_cache_cell is
 end tb_cache_cell;
 
 architecture tb of tb_cache_cell is
-	component cache_cell
+	component cache_cell_sr
     	port (
         	CE 	: in std_logic;
         	RD_WR  : in std_logic;
+			reset	:in std_logic;
         	D_in   : in std_logic;
         	D_out  : out std_logic
     	);
@@ -27,16 +28,17 @@ begin
     	port map(
         	CE  	=> CE_tb,
         	RD_WR   => RD_WR_tb,
+			reset  => reset_tb,
         	D_in	=> D_in_tb,
         	D_out   => D_out_tb
     	);
     	stim_proc: process
     	begin
     	-- Chip Enable off, write enable, 0 data
-    	CE_tb <= '0'; RD_WR_tb <= '0'; D_in_tb <= '0';
+    	CE_tb <= '0'; RD_WR_tb <= '0'; D_in_tb <= '0'; reset_tb <= '1';
     	wait for 20 ns;
     	-- Chip Enable off, read enable, 0 data
-    	CE_tb <= '0'; RD_WR_tb <= '1'; D_in_tb <= '0';
+    	CE_tb <= '0'; RD_WR_tb <= '1'; D_in_tb <= '0'; reset_tb <= '1';
     	wait for 20 ns;
    	 
     	-- Chip Enable on, write enable, 0 data,

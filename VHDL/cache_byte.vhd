744ac99098ef3451479d182cdf5067f2e69eb4d2
hannahMathew1 <hmathew1>
2025-10-31 13:19:47 -0400

    merging

--- VHDL/cache_byte.vhd
+++ VHDL/cache_byte.vhd
@@ -1,4 +1,4 @@
--- Entity: cache_cell
+-- Entity: cache_byte
 -- Architecture: structural
 -- Author: Juan Marroquin
 --
@@ -6,64 +6,50 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity cache_cell is
+entity cache_byte is
+	port (
+    	CE_index 	: in std_logic;
+    	CE_offset	: in std_logic;
+    	RD_WR  : in std_logic;
+    	D_in   : in std_logic_vector(7 downto 0);
+    	D_out  : out std_logic_vector(7 downto 0));
+end cache_byte;
+ 
+architecture structural of cache_byte is
+
+-- REQUIRED COMPONENTS
+component cache_cell is
 	port (
     	CE 	: in std_logic;
     	RD_WR  : in std_logic;
     	D_in   : in std_logic;
     	D_out  : out std_logic);
-end cache_cell;
-
-architecture structural of cache_cell is
-
--- REQUIRED COMPONENTS
-component cache_sel
-	port (
-    	CE  	: in  std_logic;
-    	RD_WR   : in  std_logic;
-    	RE  	: out std_logic;
-    	WE  	: out std_logic);
-	end component;
-
-component Dlatch_Outdated
-	port (
-  	D  	: in  std_logic;
-  	EN 	: in  std_logic;
-  	Q  	: out std_logic;
-  	Q_n	: out std_logic);
-	end component;
- 
-component tx
-	port (
-  	sel   : in  std_logic;
-  	selnot   : in  std_logic;
-  	input	: in  std_logic;
-  	output   : out std_logic);
-	end component;
-
-component inverter
+end component;
+    
+component and2
   port (
-	input   : in std_logic;
+	input1   : in  std_logic;
+	input2   : in  std_logic;
 	output   : out std_logic);
 end component;
- 
--- SIGNALS
-signal RE_signal, WE_signal: std_logic;
-signal RE_bar 	: std_logic;
-signal Q_int, Q_n_int  : std_logic;
 
-for cache_selector1 : cache_sel use entity work.cache_sel(structural);
-for Dlatch1    	: Dlatch_Outdated	use entity work.Dlatch_Outdated(structural);
-for tx1        	: tx    	use entity work.tx(structural);
-for inverter1  	: inverter  use entity work.inverter(structural);
 
+signal CE : std_logic;
 
 begin
-	cache_selector1: cache_sel port map (CE, RD_WR, RE_signal, WE_signal);
-	Dlatch1: Dlatch_Outdated port map (D_in, WE_signal, Q_int, Q_n_int);
-	inverter1: inverter port map (RE_signal, RE_bar);
-	tx1: tx port map (RE_signal, RE_bar, Q_int, D_out);
+	chip_enable: entity work.and2(structural)
+    	port map(CE_index, CE_offset, CE);
 
+	gen_cell: for i in 0 to 7 generate
+    	cell_i: entity work.cache_cell(structural)
+        	port map(
+            	CE 	=> CE,
+            	RD_WR  => RD_WR,
+            	D_in   => D_in(i),
+            	D_out  => D_out(i)
+        	);
+	end generate;
 end structural;
+
 
 

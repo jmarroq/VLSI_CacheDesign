21b89ab1819dd64c8fed386a35571e3c417a4d0a
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2025-10-31 13:46:16 -0400

    Create dff_neg.vhd

---
+++ CACHERUN/dff_neg.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity dff_neg is
+  port(
+    clk   : in  std_logic;
+    d     : in  std_logic;
+    reset : in  std_logic;  
+    q     : out std_logic
+  );
+end entity;
+
+architecture structural of dff_neg is
+  signal q_int : std_logic := '0';
+begin
+  process(clk, reset)
+  begin
+    if reset = '1' then
+      q_int <= '0';
+    elsif falling_edge(clk) then
+      q_int <= d;
+    end if;
+  end process;
+  q <= q_int;
+end architecture;

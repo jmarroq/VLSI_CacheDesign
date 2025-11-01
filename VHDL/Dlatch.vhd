0a3a4079d742fb0a775184b6f51fbbc734b12c41
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2025-10-31 20:21:32 -0400

    Update Dlatch.vhd

--- VHDL/Dlatch.vhd
+++ VHDL/Dlatch.vhd
@@ -1,33 +1,24 @@
--- Entity: positive level triggered D latch
--- Architecture : structural
--- Author: 
---
-  
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-library STD;
-library IEEE;                      
-use IEEE.std_logic_1164.all;       
+entity Dlatch is
+  port ( 
+    D   : in  std_logic;
+    EN  : in  std_logic;
+    Q   : out std_logic;
+    Q_n : out std_logic
+  );
+end Dlatch;
 
-entity Dlatch is                      
-  port ( d   : in  std_logic;
-         clk : in  std_logic;
-         q   : out std_logic;
-         qbar: out std_logic); 
-end Dlatch;                          
-
-architecture structural of Dlatch is 
-
- 
-  
+architecture behavioral of Dlatch is 
 begin
-  
-  output: process (d,clk)                  
-
-  begin                           
-    if clk = '1' then 
-    q <= d;
-    qbar <= not d ;
- end if; 
- end process output;        
-                             
-end structural;  
+  output: process (D, EN) -- Sensitivity list includes D and EN to infer a latch
+  begin
+    -- Positive level-sensitive D-Latch
+    if EN = '1' then
+      Q <= D;
+      Q_n <= not D;
+    end if; 
+   
+  end process output;
+end behavioral;

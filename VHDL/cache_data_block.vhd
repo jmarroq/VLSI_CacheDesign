2e0024160d256a629f8daa6d6d7f6daa284873de
Juan Carlos Marroquin <jmarroq2@umbc.edu>
2025-11-01 08:48:26 -0400

    Changes for cache cell

--- VHDL/cache_data_block.vhd
+++ VHDL/cache_data_block.vhd
@@ -1,59 +1,52 @@
--- Entity: cache_block
--- Architecture: structural
+--============================================================
+-- Entity: cache_data_block
 -- Author: Juan Marroquin
---
+-- Description:
+--   4-line cache data array, each line consisting of:
+--     - 4-byte data section
+--     - 2-bit tag
+--     - 1-bit valid flag
+--   Includes asynchronous reset propagation to all lines.
+--============================================================
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity cache_data_block is
-  port (
-    	CE_index 	: in std_logic_vector(3 downto 0);
-    	CE_offset	: in std_logic_vector(3 downto 0);
-    	RD_WR  : in std_logic;
-    	Tag_in : in std_logic_vector(1 downto 0);
-    	Tag_out: out std_logic_vector(1 downto 0);
-    	V_in   : in std_logic;
-    	V_out  : out std_logic;
-    	D_in   : in std_logic_vector(7 downto 0);
-    	D_out  : out std_logic_vector(7 downto 0));
+    port (
+        CE_index  : in  std_logic_vector(3 downto 0);  
+        CE_offset : in  std_logic_vector(3 downto 0);   
+        RD_WR     : in  std_logic;                      
+        reset     : in  std_logic;                      
+        Tag_in    : in  std_logic_vector(1 downto 0);   
+        Tag_out   : out std_logic_vector(1 downto 0);   
+        V_in      : in  std_logic;                      
+        V_out     : out std_logic;                      
+        D_in      : in  std_logic_vector(7 downto 0);   
+        D_out     : out std_logic_vector(7 downto 0)    
+    );
 end cache_data_block;
 
 architecture structural of cache_data_block is
-
-  -- Component Declaration (copied from your full_cache_line.vhd)
-  component full_cache_line is
-	port (
-  	CE_index 	: in  std_logic;
-  	CE_offset	: in  std_logic_vector(3 downto 0);
-  	RD_WR    	: in  std_logic;
-  	Tag_in   	: in  std_logic_vector(1 downto 0);
-  	Tag_out  	: out std_logic_vector(1 downto 0);
-  	V_in     	: in  std_logic;
-  	V_out    	: out std_logic;
-  	D_in     	: in  std_logic_vector(7 downto 0);
-  	D_out    	: out std_logic_vector(7 downto 0)
-	);
-  end component;
-
-  -- Configuration (Best Practice)
-  --for all : full_cache_line use entity work.full_cache_line(structural);
-
 begin
 
-  gen_lines: for i in 0 to 3 generate
-	line_inst : entity work.full_cache_line(structural)
-  	port map (
-    	CE_index 	=> CE_index(i),
-    	CE_offset	=> CE_offset,
-    	RD_WR    	=> RD_WR,
-    	Tag_in   	=> Tag_in,
-    	V_in     	=> V_in,
-    	D_in     	=> D_in,
-    	Tag_out  	=> Tag_out,
-    	V_out    	=> V_out,
-    	D_out    	=> D_out
-  	);
-  end generate gen_lines;
+    ----------------------------------------------------------------
+    -- Generate 4 full_cache_line blocks (one per index line)
+    ----------------------------------------------------------------
+    gen_lines: for i in 0 to 3 generate
+        line_inst: entity work.full_cache_line(structural)
+            port map (
+                CE_index  => CE_index(i),  -- one index enable per line
+                CE_offset => CE_offset,    -- shared offset select
+                RD_WR     => RD_WR,
+                reset     => reset,
+                Tag_in    => Tag_in,
+                Tag_out   => Tag_out,
+                V_in      => V_in,
+                V_out     => V_out,
+                D_in      => D_in,
+                D_out     => D_out
+            );
+    end generate gen_lines;
 
 end structural;

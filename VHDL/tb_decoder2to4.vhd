a053c0667266a420cabdab2693b251ac4b8cb806
hannahMathew1 <168139287+hannahMathew1@users.noreply.github.com>
2025-11-03 12:25:50 -0500

    Update tb_decoder2to4.vhd

--- VHDL/tb_decoder2to4.vhd
+++ VHDL/tb_decoder2to4.vhd
@@ -2,14 +2,14 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity tb_decoder_2to4 is
+entity tb_decoder2to4 is
 -- The testbench entity is typically empty
-end tb_decoder_2to4;
+end tb_decoder2to4;
 
-architecture behavior of tb_decoder_2to4 is
+architecture behavior of tb_decoder2to4 is
 
     -- Declare the component (Unit Under Test - UUT)
-    component decoder_2to4
+    component decoder2to4
     port ( EN  : in  std_logic;
    	A  : in  std_logic_vector(1 downto 0);
     	Y  : out std_logic_vector(3 downto 0)
@@ -29,7 +29,7 @@ architecture behavior of tb_decoder_2to4 is
 begin
 
     -- Instantiate the Unit Under Test (UUT)
-    uut: decoder_2to4
+    uut: decoder2to4
     port map ( EN  => tb_EN,
                A => tb_A,
                Y => tb_Y,
